77268b7ad5e9197856721a088021abf7c64d3fb2
UribesEffect <38766156+UribesEffect@users.noreply.github.com>
2021-03-03 12:07:44 -0500

    Add files via upload
    Proxy terminado

---
+++ PROXY.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 02 13:26:46 2021
+
+@author: acer
+"""
+
+class Data:
+    def __init__(self,list):
+        self.list = list
+        
+class Proxy:
+    def __init__(self, instance):
+        self.instance = instance
+
+    def proxy(self):
+        obj_inst = self.instance
+        new_inst_list = []
+        print('Proxy..')
+        for element in obj_inst.list:
+            if type(element) == float:
+                new_inst_list.append(element)
+            else:
+                new_inst_list.append(float(element))
+        self.instance.list = new_inst_list
+        print('Here you have it. :)')	
+        
+
+list_data = [0, 1, 2.5, 3, 4, 6]
+obj_data = Data(list_data)
+obj_proxy = Proxy(object_data)
+obj_proxy.proxy()
+print(obj_data.list)
+print(obj_data.list)
